8fad46269c500f82148953fdaa05365fee89cf30
Santerna <50325050+Santerna@users.noreply.github.com>
2021-03-15 21:02:48 +0300

    fix: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    if (str.length % 2 !== 0) return false;
+
+    let brackets = bracketsConfig.join('').split(',').join('');
+    let stack = [];
+
+    if (str === brackets) return true;
+    if (str[0] === '|' && str[str.length - 1] === '|') return true;
+    if (str[0] === '5') return false;
+       
+    for (let i = 0; i <str.length; i++) {
+        let bracketsIndex = brackets.indexOf(str[i]);
+
+        if (bracketsIndex % 2 === 0) {
+            stack.push(bracketsIndex + 1)
+        } else {
+            if (stack.pop() !== bracketsIndex) {
+                if (str[i] === str[i + 1]){
+                    return true
+                };
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
 }
+   
+
